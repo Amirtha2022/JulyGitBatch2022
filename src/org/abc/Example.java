de7fc28843e910b39af46072d240bb201c0de835
Amirtha2022 <greenstask052022@gmail.com>
2022-07-21 14:02:19 +0530

    Dev added code

--- src/org/abc/Example.java
+++ src/org/abc/Example.java
@@ -2,6 +2,16 @@ package org.abc;
 
 public class Example {
 
-	int a=1000;
-	
+	static int a = 500;
+
+	private void m3() {
+
+		System.out.println("Dev craeted m3");
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(a);
+	}
+
 }
